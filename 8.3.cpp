e8a3b4dbc9c9de2fb8d475c543434b3e13002e3d
deixman1 <deix@yandex.ru>
2017-04-21 00:19:09 +0700

    Revert "Update 8.3.cpp"

--- 8.3.cpp
+++ 8.3.cpp
@@ -1,31 +1,25 @@
 //8.3 by me and crusadwerei0fojxskvmjsklf
 #include <iostream>
 #include <cmath>
-#include <conio.h>
 using namespace std;
 
-int a;
+float a;
 int n;
 
-int stepen(int n, int b);
-int main()
+float stepen (int n, float b);
+int main ()
 {
-	int proiz;
-	cin >> n >> a;
-	if (n % 2 == 0) proiz = stepen(n / 2, a)*stepen(n / 2, a); else  proiz = a*stepen(n - 1, a);
-	cout << proiz;
-	_getch();
+    float proiz;
+    cin>>n>>a;
+    if (n%2 ==0) proiz=stepen(n/2,a)*stepen(n/2,a) ; else  proiz=a*stepen(n-1,a);
+    cout << proiz;
+    return 0;
 }
 
-int stepen(int n, int b)
+float stepen (int n, float b)
 {
-	if (n)
-	{
-		if (n == 1)
-			return b;
-		b *= a;
-		stepen(n - 1, b);
-	}
-	else 
-		return 1;
+    if (n)
+     {
+     b=pow(b,n); stepen(n-1,b);
+     } else return(b);
 }
