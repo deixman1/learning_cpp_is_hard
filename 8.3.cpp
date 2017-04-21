9c66f05c47a6952c62c10cf40d90431a2c0abd92
deixman1 <deix@yandex.ru>
2017-04-21 09:56:53 +0700

    Update 8.3.cpp

--- 8.3.cpp
+++ 8.3.cpp
@@ -1,23 +1,22 @@
 //8.3 by me and crusadwerei0fojxskvmjsklf
 #include <iostream>
-#include <cmath>
 #include <conio.h>
 using namespace std;
 
 int a;
 int n;
 
-int stepen(int n, int b);
+long long int stepen(int n, int b);
 int main()
 {
-	int proiz;
+	long long int proiz;
 	cin >> n >> a;
 	if (n % 2 == 0) proiz = stepen(n / 2, a)*stepen(n / 2, a); else  proiz = a*stepen(n - 1, a);
 	cout << proiz;
 	_getch();
 }
 
-int stepen(int n, int b)
+long long int stepen(int n, int b)
 {
 	if (n)
 	{
